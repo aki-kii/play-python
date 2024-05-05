2d119c906511f1a606919e4443a412beb0f55b26
akicitron308 <impeeeeedance@icloud.com>
2024-05-05 19:48:39 +0900

    Challenge001-01: CelsiusModel, FahrenheitModelのリファクタリング

--- src/challenge_001/celsius_model.py
+++ src/challenge_001/celsius_model.py
@@ -1,14 +1,25 @@
 from pydantic import BaseModel, Field
 
+ABSOLUTE_TEMPERATURE = -273.15
+"""絶対零度の温度"""
+
+ROUND_DECIMAL = 2
+"""小数点以下の有効桁数"""
+
 
 class CelsiusModel(BaseModel):
     """セルシウス温度のモデル"""
 
     temperature: float = Field(
         ...,
-        gt=-273.15,
+        ge=ABSOLUTE_TEMPERATURE,
     )
+    """温度"""
 
     def to_fahrenheit(self) -> float:
-        fahrenheit = self.temperature * 1.8 + 32
-        return fahrenheit
+        """ファーレンハイト温度に変換する
+
+        Returns:
+            float: ファーレンハイト温度
+        """
+        return round(self.temperature * 1.8 + 32, ROUND_DECIMAL)
--- src/challenge_001/fahrenheit_model.py
+++ src/challenge_001/fahrenheit_model.py
@@ -1,10 +1,25 @@
 from pydantic import BaseModel, Field
 
+ABSOLUTE_TEMPERATURE = -459.67
+"""絶対零度の温度"""
+
+ROUND_DECIMAL = 2
+"""小数点以下の有効桁数"""
+
 
 class FahrenheitModel(BaseModel):
     """ファーレンハイト温度のモデル"""
 
     temperature: float = Field(
         ...,
-        gt=-459.67,
+        ge=ABSOLUTE_TEMPERATURE,
     )
+    """温度"""
+
+    def to_celsius(self) -> float:
+        """セルシウス温度に変換する
+
+        Returns:
+            float: セルシウス温度
+        """
+        return round((self.temperature - 32) * 5 / 9, ROUND_DECIMAL)
